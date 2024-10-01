d37cd11b2510277f315d9627ce46f74eed9fde08
filipton <filipton12@gmail.com>
2024-10-01 22:36:53 +0200

    wip: websocket key parser

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,13 @@
 use anyhow::Result;
 
 pub fn start_client(ip: &str) -> Result<()> {
+    println!("Trying to connect to: {ip}...");
+    let (mut socket, resp) = tungstenite::connect(ip)?;
+
+    println!("Http response: {resp:?}");
+    socket.send(tungstenite::Message::Text("Lorem".into()))?;
+    std::thread::sleep(std::time::Duration::from_secs(5));
+    socket.close(None)?;
+
     Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,14 @@
+use anyhow::Result;
+use base64::prelude::*;
+use sha1::{Digest, Sha1};
 use std::net::TcpListener;
 
-use anyhow::Result;
-
 pub fn start_server(ip: &str) -> Result<()> {
+    println!(
+        "dsa: {:?}",
+        parse_sec_websocket_key("dGhlIHNhbXBsZSBub25jZQ==")
+    );
+
     let listener = TcpListener::bind(ip)?;
     println!("Server started on: {ip:?}!");
 
@@ -11,4 +17,12 @@ pub fn start_server(ip: &str) -> Result<()> {
     }
 
     Ok(())
+}
+
+const WS_KEY_GUID: &'static str = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+fn parse_sec_websocket_key(key: &str) -> String {
+    let tmp = format!("{key}{WS_KEY_GUID}");
+    let mut hasher = Sha1::new();
+    hasher.update(tmp.as_bytes());
+    BASE64_STANDARD.encode(hasher.finalize())
 }
